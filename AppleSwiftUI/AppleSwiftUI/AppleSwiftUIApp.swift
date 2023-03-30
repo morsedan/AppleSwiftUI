91a801d413080583e8bfcf8a9c4f20428fe822d0
Dan Morse <dmorse@adobe.com>
2023-03-30 11:16:46 -0700

    Completed

---
+++ AppleSwiftUI/AppleSwiftUI/AppleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSwiftUIApp.swift
+//  AppleSwiftUI
+//
+//  Created by Dan Morse on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
